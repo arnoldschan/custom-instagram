a248e3672fce0e0de2db0d7bd4e9a08bb2d5c250
Arnold <arnold.chan@lndata.com>
2020-10-13 19:18:52 +0800

    feat: :sparkles: signup ability

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { db } from "./firebase/firebase";
+import { db, auth } from "./firebase/firebase";
 import './App.css';
 import Post from './Post';
-import Auth from './Auth';
+import AuthModal from './Auth';
+import { Button } from "@material-ui/core";
 
 function App() {
+  const IG_LOGO = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
   const [posts, setPosts] = useState([])
+  const [openModal, setOpenModal] = useState(false)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser)=>{
+      if (authUser){
+        console.log(authUser)
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }})
+    return () => {
+      unsubscribe();
+    }
+  }, [ user ])
+
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot=>{
       setPosts(snapshot.docs.map(doc=> (
@@ -17,10 +35,11 @@ function App() {
   }, [])
   return (
     <div className="app">
+    <AuthModal igLogo={IG_LOGO} openModal={openModal} setOpenModal={setOpenModal}/>
       <div className="app__header">
-        <img className="app__headerImage"src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo"/>
+        <img className="app__headerImage"src={IG_LOGO} alt="instagram logo"/>
       </div>
-      <Auth/>
+      <Button onClick={() => setOpenModal(true)}>SignUp</Button>
       {
       posts.map( ({id, post}) => (
         <Post key={id} username={post.username} caption={post.caption} imageURL={post.imageURL}/>
--- src/Auth.js
+++ src/Auth.js
@@ -1,23 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth } from "./firebase/firebase";
 import { Modal } from "@material-ui/core";
+import { Input, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import './Auth.css';
 
-function Auth() {
+function Auth({ igLogo, openModal, setOpenModal }) {
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+          position: 'absolute',
+          width: 400,
+          backgroundColor: theme.palette.background.paper,
+          border: '2px solid #000',
+          boxShadow: theme.shadows[5],
+          padding: theme.spacing(2, 4, 3),
+        },
+      }));
+
+    const getModalStyle = () => {
+        const top = 50;
+        const left = 50;
+        return {
+            top: `${top}%`,
+            left: `${left}%`,
+            transform: `translate(-${top}%, -${left}%)`,
+        };
+    }
+    const classes = useStyles();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [modalStyle] = React.useState(getModalStyle);
+    const registerUser = () => {
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((authUser)=>{
+            authUser.user.updateProfile({
+                displayName: username
+            })
+        })
+        .catch((error)=> alert(error.message));
+    }
     return (
         <Modal
-            disablePortal
-            disableEnforceFocus
-            disableAutoFocus
-            close
-            aria-labelledby="server-modal-title"
-            aria-describedby="server-modal-description"
+            open={openModal}
+            onClose={()=>setOpenModal(false)}
             >
-            <div>
-                <h2 id="server-modal-title">Server-side modal</h2>
-                <p id="server-modal-description">If you disable JavaScript, you will still see me.</p>
+            <div style={modalStyle} className={classes.paper}>
+                <form className="auth__form">
+                    <center>
+                    <img src={igLogo} alt="instagram-logo"/>
+                    </center>
+                    <Input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} type="email"/>
+                    <Input placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)} type="text"/>
+                    <Input placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} type="password"/>
+                    <Button onClick={()=>registerUser()}>Sign Up</Button>
+                </form>
             </div>
         </Modal>
     )
 }
+
 
 export default Auth
 
