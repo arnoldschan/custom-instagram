03de91a9d8d3af2d831b0bb558c7f80353993edf
Arnold <arnold.chan@lndata.com>
2020-10-13 19:18:52 +0800

    feat: base modal, unstable
    unstable

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { db } from "./firebase/firebase";
 import './App.css';
 import Post from './Post';
 import Auth from './Auth';
+import { Button } from "@material-ui/core";
 
 function App() {
   const [posts, setPosts] = useState([])
+  const [openModal, setOpenModal] = useState(false)
+
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot=>{
       setPosts(snapshot.docs.map(doc=> (
@@ -17,10 +20,11 @@ function App() {
   }, [])
   return (
     <div className="app">
+    <Auth openModal={openModal} setOpenModal={setOpenModal}/>
       <div className="app__header">
         <img className="app__headerImage"src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo"/>
       </div>
-      <Auth/>
+      <Button onClick={() => setOpenModal(true)}>SignUp</Button>
       {
       posts.map( ({id, post}) => (
         <Post key={id} username={post.username} caption={post.caption} imageURL={post.imageURL}/>
--- src/Auth.js
+++ src/Auth.js
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from "@material-ui/core";
+import { Input, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
-function Auth() {
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  
+function Auth({ openModal, setOpenModal }) {
+    const classes = useStyles();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [modalStyle] = React.useState(getModalStyle);
     return (
         <Modal
-            disablePortal
-            disableEnforceFocus
-            disableAutoFocus
-            close
-            aria-labelledby="server-modal-title"
-            aria-describedby="server-modal-description"
+            open={openModal}
+            onClose={()=>setOpenModal(false)}
             >
-            <div>
+            <div style={modalStyle} className={classes.paper}>
                 <h2 id="server-modal-title">Server-side modal</h2>
-                <p id="server-modal-description">If you disable JavaScript, you will still see me.</p>
+                <Input placeholder={email} value={email} onChangeText={setEmail}/>
+                <Input placeholder={username} value={username} onChangeText={setUsername}/>
+                <Input placeholder={password} value={password} onChangeText={setPassword}/>
+                <Button/>
             </div>
         </Modal>
     )
+}
+
+const getModalStyle = () => {
+    const top = 50;
+    const left = 50;
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `transform(-${top})%, -${left}%`,
+    };
 }
 
 export default Auth
